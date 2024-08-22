b5bc65ea613a24b8e2f923c2a99c5c7c963fad39
2paperstar <lee@paperst.ar>
2024-08-22 23:48:28 +0900

    feat: init

---
+++ src/main/kotlin/me/gistory/newbies_server_24/ServerFor2024NewbiesApplication.kt
@@ -0,0 +1,11 @@
+package me.gistory.newbies_server_24
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerFor2024NewbiesApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerFor2024NewbiesApplication>(*args)
+}
---
+++ src/test/kotlin/me/gistory/newbies_server_24/ServerFor2024NewbiesApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.gistory.newbies_server_24
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerFor2024NewbiesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
