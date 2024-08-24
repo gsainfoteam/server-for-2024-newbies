a7bd1a67fad7418f0af229eb2b02d7c15d6b9db5
2paperstar <lee@paperst.ar>
2024-08-25 07:29:34 +0900

    fix: separate API `publishedAt` as field, so publishedAt surely returns when application is published

--- src/main/kotlin/me/gistory/newbies_server_24/controllers/RootController.kt
+++ src/main/kotlin/me/gistory/newbies_server_24/controllers/RootController.kt
@@ -3,19 +3,20 @@ package me.gistory.newbies_server_24.controllers
 import io.swagger.v3.oas.annotations.tags.Tag
 import me.gistory.newbies_server_24.dto.ApiInfoDto
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
-@Tag(name="Api info")
+@Tag(name = "Api info")
 @RestController
 class RootController {
+    private val publishedAt = Date()
+
     @GetMapping("/")
     fun getApiInfo(): ApiInfoDto {
         return ApiInfoDto(
             name = "Newbies API",
             version = "1.0.0",
-            publishedAt = Date(),
+            publishedAt = publishedAt,
         )
     }
 }
