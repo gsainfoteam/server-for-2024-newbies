cb961e2a756d7db412db8266e12b128acbad37be
2paperstar <lee@paperst.ar>
2024-08-25 13:16:04 +0900

    refactor: use Kotlin DSL to define SecurityConfiguration

--- src/main/kotlin/me/gistory/newbies_server_24/configurations/SecurityConfiguration.kt
+++ src/main/kotlin/me/gistory/newbies_server_24/configurations/SecurityConfiguration.kt
@@ -3,40 +3,56 @@ package me.gistory.newbies_server_24.configurations
 import me.gistory.newbies_server_24.providers.TokenProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatchers
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 class SecurityConfiguration {
     @Bean
     fun passwordEncoder() = BCryptPasswordEncoder()
 
     @Bean
-    fun securityFilterChain(httpSecurity: HttpSecurity, tokenProvider: TokenProvider): SecurityFilterChain =
-        httpSecurity
-            .csrf { csrf -> csrf.disable() }
-            .cors(Customizer.withDefaults())
-            .headers { headers ->
-                headers.frameOptions { it.sameOrigin() }
+    fun securityFilterChain(httpSecurity: HttpSecurity, tokenProvider: TokenProvider): SecurityFilterChain {
+        httpSecurity.invoke {
+            csrf { disable() }
+            cors {
+                configurationSource = CorsConfigurationSource { CorsConfiguration().applyPermitDefaultValues() }
             }
-            .formLogin { form -> form.disable() }
-            .httpBasic { basic -> basic.disable() }
-            .sessionManagement { session ->
-                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            headers {
+                frameOptions { sameOrigin = true }
             }
-            .authorizeHttpRequests { req ->
-                req.requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/api/**").permitAll()
-                req.requestMatchers("/auth/login", "/auth/register").permitAll()
-                req.requestMatchers("/error").permitAll()
-                req.requestMatchers("/").permitAll()
-                req.anyRequest().authenticated()
+            formLogin { disable() }
+            httpBasic { disable() }
+            sessionManagement {
+                sessionCreationPolicy = SessionCreationPolicy.STATELESS
             }
-            .addFilterBefore(JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter::class.java)
-            .build()
+            authorizeHttpRequests {
+                authorize("/swagger-ui/**", permitAll)
+                authorize("/v3/api-docs/**", permitAll)
+                authorize("/api/**", permitAll)
+                authorize("/error", permitAll)
+                authorize(
+                    RequestMatchers.anyOf(
+                        AntPathRequestMatcher.antMatcher("/auth/**"),
+                        AntPathRequestMatcher.antMatcher("/"),
+                    ), permitAll
+                )
+                authorize(anyRequest, authenticated)
+            }
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtFilter(tokenProvider))
+        }
+        return httpSecurity.build()
+    }
 }
