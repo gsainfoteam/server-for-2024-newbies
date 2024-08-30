66804bbb0e25e159b5f0ed0fb52da1aae692bd2a
2paperstar <lee@paperst.ar>
2024-08-30 21:50:32 +0900

    fix: invoke unauthorized exception when token parse failed

--- src/main/kotlin/me/gistory/newbies_server_24/configurations/JwtFilter.kt
+++ src/main/kotlin/me/gistory/newbies_server_24/configurations/JwtFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import me.gistory.newbies_server_24.exceptions.UnauthorizedException
 import me.gistory.newbies_server_24.providers.TokenProvider
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.GenericFilterBean
@@ -11,12 +12,11 @@ import org.springframework.web.filter.GenericFilterBean
 class JwtFilter(private val tokenProvider: TokenProvider) : GenericFilterBean() {
     override fun doFilter(req: ServletRequest, res: ServletResponse, chain: FilterChain) {
         val request = req as HttpServletRequest
-        val token = resolveToken(request)
-        token?.let {
-            if (tokenProvider.validateToken(it)) {
-                tokenProvider.getAuthentication(it).let { authentication ->
-                    SecurityContextHolder.getContext().authentication = authentication
-                }
+        val token = resolveToken(request) ?: throw UnauthorizedException()
+        token.let {
+            if (!tokenProvider.validateToken(it)) throw UnauthorizedException()
+            tokenProvider.getAuthentication(it).let { authentication ->
+                SecurityContextHolder.getContext().authentication = authentication
             }
         }
         chain.doFilter(req, res)
